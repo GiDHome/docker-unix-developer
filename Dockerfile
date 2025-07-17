1522f2cbc832f47eddd3296d4b69094e6444b569
Javi Garate <fjgarate@cimne.upc.edu>
2025-07-17 22:17:22 +0200

    update gid version

--- Dockerfile
+++ Dockerfile
@@ -24,4 +24,19 @@ RUN apt-get update && apt-get -y install \
 WORKDIR /tmp
 COPY ./install-gid.sh "install-gid.sh"
 RUN chmod 750 "./install-gid.sh"
-RUN "./install-gid.sh"
+# Set the version of GiD to install as first parameter
+ARG GID_VERSION=17.1.5d
+
+RUN echo "Installing GiD version ${GID_VERSION}..."
+
+# Install GiD with the provided version
+RUN ./install-gid.sh ${GID_VERSION}
+
+#clean up
+RUN rm -rf /tmp/* && \
+	apt-get clean && \
+	apt-get autoclean && \
+	rm -rf /var/lib/apt/lists/*
+
+# docker build --progress=plain --build-arg GID_VERSION=17.1.5d -t gid:17.1.5d .
+# docker run -it --rm -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY gid:17.1.5d
--- install-gid.sh
+++ install-gid.sh
@@ -1,16 +1,20 @@
 # GiD folder in the root
 #cd /tmp
 
-# Version to check
-VERSION=15.1.1d
+# Version to check comes from the first argument
+VERSION=$1
 # Get the file
 TARFILENAME=gid${VERSION}-linux-x64.tar.xz
-wget --quiet https://www.gidhome.com/archive/GiD_Developer_Versions/Linux/amd64/${TARFILENAME}
+echo "Downloading GiD version ${VERSION} from https://downloads.gidsimulation.com/GiD_Developer_Versions/Linux/amd64/${TARFILENAME}"
+wget --quiet https://downloads.gidsimulation.com/GiD_Developer_Versions/Linux/amd64/${TARFILENAME}
+echo "Downloaded ${TARFILENAME}"
 ls
 # Unzip
+echo "Unzipping ${TARFILENAME}"
 tar -Jxf ${TARFILENAME}
 rm ${TARFILENAME}
 
+echo "Moving files to /gid"
 mv gid${VERSION}-x64/* /gid
 # cd /gid
 # PREFERENCESFOLDER=$HOME/.gid/${VERSION}/
